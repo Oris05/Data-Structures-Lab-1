557fa8a108f5b04021a5f6e6f1bca886f3767ef1
Orestas Zeigis <B00172214@mytudublin.ie>
2026-09-23 21:44:45 +0100

    Testing the remove all method

--- Lab1/TestClass.java
+++ Lab1/TestClass.java
@@ -12,6 +12,10 @@ public class TestClass {
 		System.out.println("After adding items the size is now :"+list.size());
 		//test remove
 		list.remove(1);
-		System.out.println("After removing an item th list size is now :"+list.size());
+		System.out.println("After removing an item the list size is now :"+list.size());
+		//test remove all
+		list.removeAll();
+		System.out.println("After everything was removed the list size is now :"+list.size());
+		System.out.println("All tests complete");
 	}
 }
