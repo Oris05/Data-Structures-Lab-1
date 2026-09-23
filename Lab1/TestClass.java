d8570592d404c139dd77b9de47d10b9ba88952bc
Orestas Zeigis <B00172214@mytudublin.ie>
2026-09-23 21:35:33 +0100

    Testing is the list empty

--- Lab1/TestClass.java
+++ Lab1/TestClass.java
@@ -1,6 +1,9 @@
 
 public class TestClass {
 	public static void main(String[]args) {
-		System.out.print("Hello");
+		ListArrayBased list = new ListArrayBased();
+		System.out.println("Testing an Array based ADT List");
+		//testing isEmpty
+		System.out.println("Is the the list empty? "+list.isEmpty());
 	}
 }
