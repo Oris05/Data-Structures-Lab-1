4733dcedbdae24b2748ff54fa9451db296cb710a
Orestas Zeigis <B00172214@mytudublin.ie>
2026-09-23 21:39:39 +0100

    Testing the add method

--- Lab1/TestClass.java
+++ Lab1/TestClass.java
@@ -5,5 +5,10 @@ public class TestClass {
 		System.out.println("Testing an Array based ADT List");
 		//testing isEmpty
 		System.out.println("Is the the list empty? "+list.isEmpty());
+		//test add
+		list.add(1, "car");
+		list.add(2, "bike");
+		list.add(3, "ship");
+		System.out.print("After adding items the size is now :"+list.size());
 	}
 }
